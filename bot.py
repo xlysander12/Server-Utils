9adb4724b53afecce51bae697f049dc7af7d10fd
xlysander12 <xlysander12pt@gmail.com>
2020-08-11 00:57:37 +0100

    Added logs to actions made with the bot #1

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import json
 import os
 import random
+from datetime import date
+from datetime import datetime
 
 import discord
 from discord import Member
 from discord import Role
+from discord import TextChannel
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -27,14 +30,19 @@ def get_adminrole(message):
     return role.id
 
 
+def get_logschannel(message):
+    with open("logs.json") as f:
+        channels = json.load(f)
+    channel: TextChannel = discord.utils.get(message.guild.text_channels, id=channels[str(message.guild.id)])
+    return channel.id
+
+
 bot = commands.Bot(command_prefix=get_prefix)
 
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
-    print(get_adminrole)
-    print(get_prefix)
 
 
 @bot.event
@@ -74,6 +82,13 @@ async def on_guild_remove(guild):
 
     with open("admins.json", 'w') as f:
         json.dump(admins, f, indent=4)
+
+    with open("logs.json") as f:
+        channels = json.load(f)
+    channels.pop(str(guild.id))
+
+    with open("logs.json", 'w') as f:
+        json.dump(channels, f, indent=4)
 
 
 @bot.command(name='random', help="Responds with a random number beetween a and b")
@@ -141,12 +156,34 @@ async def changeadmin(ctx, *, role: Role):
     await ctx.send(f"Admin role changed to {role.mention}")
 
 
+@bot.command(name="changelogschannel", help="Choose in which channel will bot's logs be in")
+async def changelogs(ctx, channel: TextChannel):
+    admin_role_id = get_adminrole(ctx)
+    if ctx.author not in ctx.guild.get_role(admin_role_id).members:
+        await ctx.send("You are not an admin!")
+        return
+
+    with open("logs.json") as f:
+        channels = json.load(f)
+
+    channels[str(ctx.guild.id)] = channel.id
+
+    with open("logs.json", 'w') as f:
+        json.dump(channels, f, indent=4)
+
+    await ctx.send(f"Logs channel changed to {channel.mention}")
+
+
 @bot.command(name="kick", help="Kicks a member from the server")
 async def kickmember(ctx, target: Member, *, reason="None"):
     admin_role_id = get_adminrole(ctx)
     if ctx.author not in ctx.guild.get_role(admin_role_id).members:
         await ctx.send("You are not an admin!")
         return
+
+    fdate = date.today().strftime('%d/%m/%Y')
+    now = datetime.now().strftime('%H:%M:%S')
+
     invite = await ctx.channel.create_invite()
     channel = await target.create_dm()
     # await channel.send("You have been kicked from the server " + ctx.guild.name)
@@ -154,23 +191,36 @@ async def kickmember(ctx, target: Member, *, reason="None"):
     # await channel.send(f"Kicked by: {ctx.message.author.name}")
     # await channel.send(f"Consider this as an warning... You can re-enter the server here: {invite}")
 
-    embed = discord.Embed(
+    clientembed = discord.Embed(
         title="You have been kicked",
         colour=discord.Color.orange()
     )
 
-    embed.set_footer(text="Server Utils Bot")
-    embed.add_field(name='You have been kicked from the server', value=ctx.guild.name, inline=True)
-    embed.add_field(name='Reason of the kick', value=reason, inline=True)
-    embed.add_field(name='You were kicked by', value=ctx.message.author.name, inline=False)
-    embed.add_field(name='Consider yourself warned... You can re-enter here', value=invite, inline=True)
+    clientembed.set_footer(text="Server Utils Bot")
+    clientembed.add_field(name='You have been kicked from the server', value=ctx.guild.name, inline=True)
+    clientembed.add_field(name='Reason of the kick', value=reason, inline=True)
+    clientembed.add_field(name='You were kicked by', value=ctx.message.author.name, inline=False)
+    clientembed.add_field(name='Consider yourself warned... You can re-enter here', value=invite, inline=True)
     # await channel.send(f"You have been kicked from the server {ctx.guild.name}\n"
     # f"With the following reason: {reason}\n"
     # f"Kicked by: {ctx.message.author.name}\n"
     # f"Consider this as an warning... You can re-enter the server here: {invite}")
 
-    await channel.send(embed=embed)
+    await channel.send(embed=clientembed)
     await target.kick(reason=reason)
+
+    logsembed = discord.Embed(
+        title="A member has been kicked",
+        colour=discord.Color.orange()
+    )
+
+    logsembed.set_footer(text=f"{ctx.message.author.name} at {now} of {fdate}")
+    logsembed.add_field(name="Kicked member", value=target.mention, inline=True)
+    logsembed.add_field(name="Reason", value=reason, inline=True)
+
+    logschannel = discord.utils.get(ctx.guild.text_channels, id=get_logschannel(ctx))
+    await logschannel.send(embed=logsembed)
+
     await ctx.send(f"User {target} was kicked from the server")
 
 
@@ -180,20 +230,38 @@ async def banmember(ctx, target: Member, reason='None'):
     if ctx.author not in ctx.guild.get_role(admin_role_id).members:
         await ctx.send("You are not an admin!")
         return
+
+    fdate = date.today().strftime('%d/%m/%Y')
+    now = datetime.now().strftime('%H:%M:%S')
+
     channel = await target.create_dm()
-    embed = discord.Embed(
+    clientembed = discord.Embed(
         title="You have been banned",
         colour=discord.Color.red()
     )
 
-    embed.set_footer(text="Server Utils Bot")
-    embed.add_field(name="You have been BANNED from the server", value=ctx.guild.name, inline=False)
-    embed.add_field(name='Reason of the ban', value=reason, inline=True)
-    embed.add_field(name="You were banned by", value=ctx.message.author.name, inline=True)
+    clientembed.set_footer(text="Server Utils Bot")
+    clientembed.add_field(name="You have been BANNED from the server", value=ctx.guild.name, inline=False)
+    clientembed.add_field(name='Reason of the ban', value=reason, inline=True)
+    clientembed.add_field(name="You were banned by", value=ctx.message.author.name, inline=True)
 
-    await channel.send(embed=embed)
+    await channel.send(embed=clientembed)
     await target.ban(reason=reason)
     await ctx.send(f"User {target} was banned from the server")
+
+    logsembed = discord.Embed(
+        title="A member has been BANNED",
+        colour=discord.Color.orange()
+    )
+
+    logsembed.set_footer(text=f"{ctx.message.author.name} at {now} of {fdate}")
+    logsembed.add_field(name="Banned member", value=target.mention, inline=True)
+    logsembed.add_field(name="Reason", value=reason, inline=True)
+
+    logschannel = discord.utils.get(ctx.guild.text_channels, id=get_logschannel(ctx))
+    await logschannel.send(embed=logsembed)
+
+    await ctx.send(f"User {target} was kicked from the server")
 
 
 bot.run(TOKEN)
